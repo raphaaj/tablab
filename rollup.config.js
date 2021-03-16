f555001ad6ab0115a99b6459419a358765ede75a
raphael.jorge <27439414+raphael-jorge@users.noreply.github.com>
2021-03-15 23:58:24 -0300

    build: add rollup bundler
    - set typescript target to ES5
    - add bundle configurations for commonjs, umd, global and es modules, with
      minified options
    - add bundle configuration for declarations

---
+++ rollup.config.js
@@ -0,0 +1,63 @@
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
+
+const terserPlugin = terser({
+  format: {
+    comments: 'all',
+  },
+});
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'lib/index.cjs.js',
+        format: 'cjs',
+      },
+      {
+        file: 'lib/index.cjs.min.js',
+        format: 'cjs',
+        plugins: [terserPlugin],
+      },
+      {
+        file: 'lib/index.es.js',
+        format: 'es',
+      },
+      {
+        file: 'lib/index.es.min.js',
+        format: 'es',
+        plugins: [terserPlugin],
+      },
+      {
+        file: 'lib/index.umd.js',
+        format: 'umd',
+        name: 'Tablab',
+      },
+      {
+        file: 'lib/index.umd.min.js',
+        format: 'umd',
+        name: 'Tablab',
+        plugins: [terserPlugin],
+      },
+      {
+        file: 'lib/index.global.js',
+        format: 'iife',
+        name: 'Tablab',
+      },
+      {
+        file: 'lib/index.global.min.js',
+        format: 'iife',
+        name: 'Tablab',
+        plugins: [terserPlugin],
+      },
+    ],
+    plugins: [typescript({ tsconfig: './tsconfig.build.json' })],
+  },
+  {
+    input: 'lib/index.d.ts',
+    output: [{ file: 'lib/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
